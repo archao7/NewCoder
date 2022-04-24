423564e0b2e4a74b26f8b3f3ae44ca9ce3be00c1
archao7 <765134563@qq.com>
2022-04-24 16:45:37 +0800

    4

--- src/main/java/com/example/nowcoder/dao/DiscussPostMapper.java
+++ src/main/java/com/example/nowcoder/dao/DiscussPostMapper.java
@@ -1,18 +1,21 @@
 package com.example.nowcoder.dao;
 
-
 import com.example.nowcoder.entity.DiscussPost;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+
 @Mapper
+@Repository
 public interface DiscussPostMapper {
+
     List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
 
-    //@Param()用于给参数取别名；
-    // 如果sql语句要用到参数并且函数只有一个参数时，必须要取别名，否则出错
+    // @Param注解用于给参数取别名,
+    // 如果只有一个参数,并且在<if>里使用,则必须加别名.
     int selectDiscussPostRows(@Param("userId") int userId);
 
     int insertDiscussPost(DiscussPost discussPost);
